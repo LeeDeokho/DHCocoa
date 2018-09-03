5a0ee81e5c55bb936c5daed635e1789a8430f01f
LeeDeokho <emeralho@gmail.com>
2018-09-03 17:22:49 +0900

    from dynamic to static2

---
+++ Frameworks/TestFramework.framework/Headers/objcTest.h
@@ -0,0 +1,20 @@
+//
+//  objcTest.h
+//  objcTest
+//
+//  Created by Hyup on 2018. 9. 3..
+//  Copyright © 2018년 NHN Entertainment Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIViewController+ObjcTest.h"
+
+//! Project version number for objcTest.
+FOUNDATION_EXPORT double objcTestVersionNumber;
+
+//! Project version string for objcTest.
+FOUNDATION_EXPORT const unsigned char objcTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcTest/PublicHeader.h>
+
+
